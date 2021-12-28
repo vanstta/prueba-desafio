9dcbebec5892844ec1e546317cbbf431fad0a99b
Vanina Scilletta <Vanina@ARRMS-PF36PDBS.valtech.com>
2021-12-28 14:17:25 -0300

    agrego paginación

--- src/js/llamado-api.js
+++ src/js/llamado-api.js
@@ -11,36 +11,6 @@ export const obtenerUsuarios = async() => {
    const data = await respuesta.json();
    const usuarios = data.results
    
-   const getPaginado = (usuarios, pages) => {
-       const page = pages || 1;
-       const limit = 10; 
-       const offset= (page -1) * limit; 
-       const usuariosPaginados = usuarios.slice(offset).slice(0, limit)
-       
-       const total_pages = Math.ceil(usuarios.length / limit);
-      
-       return {usuariosPaginados, total_pages}
-    }
-       
-  const body  = document.body;
-
-   const {total_pages} = getPaginado(usuarios, 1);
-
-   for (let i =1; i< total_pages +1; i++) {
-
-      console.log(getPaginado(usuarios, `${i}`))
-       const button = document.createElement('button')
-       button.innerHTML = `${i}`;
-       body.appendChild( button );
-       button.classList.add('botones-paginado');
-       
-       if (i == total_pages) {
-           // accionarBotones()
-       }
-   }    
-
-
-
     return usuarios;
 
    }catch(err) {
@@ -51,3 +21,13 @@ export const obtenerUsuarios = async() => {
 
 }
 
+export const getPaginado = (usuarios, pages) => {
+    const page = pages || 1;
+    const limit = 10; 
+    const offset= (page -1) * limit; 
+    const usuariosPaginados = usuarios.slice(offset).slice(0, limit)
+    
+    const total_pages = Math.ceil(usuarios.length / limit);
+   
+    return {usuariosPaginados, total_pages}
+ }
--- src/js/usuarios.js
+++ src/js/usuarios.js
@@ -1,4 +1,4 @@
-import {obtenerUsuarios} from './llamado-api'
+import {obtenerUsuarios, getPaginado} from './llamado-api'
 
 const body  = document.body;
 let card;
@@ -18,46 +18,6 @@ const hacerHtml = () => {
     card=document.getElementById('card')
    
 
-
-}
-
-const crearCardUsuario = ( usuario ) => {
-    const {
-        picture: {large},
-        name: {first},
-        name: {last},
-        location: {city},
-        location: {state},
-        location: {postcode},
-        registered: {age}
-
-        } = usuario
-
-    const html = `
-    <img class="foto" src="${large}" alt="persona">
-    <div class="cardDatos">
-        <h3 class="nombre">${first} ${last}</h3>
-        <i class="fas fa-filter puesto">Recruitment Consultancy</i>
-        <i class="fas fa-map-marker-alt ciudad">${city}, ${state} ${postcode} </i>
-        <hr>
-        <div>
-            <i class="far fa-clock activo">Activo hace: ${age} años</i>
-            <i class="far fa-heart"></i>
-        </div>
-        
-        
-    </div>
-    `;
-
-    const divCard = document.createElement('div');
-    divCard.classList.add('user')
-    divCard.innerHTML = html;
-
-    card.appendChild(divCard)
-    
-    
-    
-
 }
 
 export const init = async() => {
@@ -66,12 +26,76 @@ export const init = async() => {
    
 
     const usuarios = await obtenerUsuarios();
-    usuarios.forEach(crearCardUsuario);
+   
+    const {total_pages, usuariosPaginados} = getPaginado(usuarios, 1);
+    let dataUsuario = usuariosPaginados;
+    crearCardUsuario(dataUsuario)
 
+    // creo los botones y llamo a la función getPaginado para que se actualicen con la data recibida
+
+    for (let i =1; i< total_pages +1; i++) {
+
+
+         const divButtons = document.querySelector('.buttonContainer')
+         const button = document.createElement('button')
+         button.innerHTML = `${i}`;
+         divButtons.appendChild(button)
+         button.classList.add('botonesPaginado');
+         divButtons.classList.add('buttonContainer');
+         body.appendChild(divButtons)
+
+         button.onclick= function () {
+            dataUsuario = getPaginado(usuarios, i ).usuariosPaginados
+            crearCardUsuario(dataUsuario)
+         }
+     }    
 }
 
+const crearCardUsuario = (dataUsuario) => {
+
+    const usuarioContainer = document.getElementById("card");
+  //hago un map para recorrer la data recibida y generar la card con los datos solicitados en el maquetado
+  
+  
+  usuarioContainer.innerHTML = dataUsuario
+    // const {
+    //     picture: {large},
+    //     name: {first},
+    //     name: {last},
+    //     location: {city},
+    //     location: {state},
+    //     location: {postcode},
+    //     registered: {age}
+
+    //     } = usuario
+      .map(
+        
+        ({ name, picture, location, registered }) => ` <div class="cardUsuario"> 
+        <img class="foto" src="${picture.large}" alt="persona">
+        <div class="cardDatos">
+            <h3 class="nombre">${name.first} ${name.last}</h3>
+            <i class="fas fa-filter puesto">Recruitment Consultancy</i>
+            <i class="fas fa-map-marker-alt ciudad">${location.city}, ${location.state} ${location.postcode} </i>
+            <hr>
+            <div>
+                <i class="far fa-clock activo">Activo hace: ${registered.age} años</i>
+                <i class="far fa-heart"></i>
+            </div>
+            
+        </div>
+        </div>`
+  
+      )
+  
+      .join("");
+  
+    console.log("user::", dataUsuario);
+  
+  };
+
+
 const inputBuscar =document.getElementById('buscar')
-const tarjeta = document.getElementsByClassName('user')
+const tarjeta = document.getElementsByClassName('cardUsuario')
 
 inputBuscar.addEventListener('keyup', (e)=> {
     let texto= e.target.value;
